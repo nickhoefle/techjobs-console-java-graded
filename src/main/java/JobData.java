8cc449002fa9fc4c57287a0c4c1e6f4ea72c281f
Nick Hoefle <nickhoefle@gmail.com>
2022-09-03 23:19:07 -0500

    Part #1 complete

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -79,7 +76,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -94,12 +91,20 @@ public class JobData {
      * @return      List of all jobs with at least one field containing the value
      */
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
-
         // load data, if not already loaded
         loadData();
-
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
         // TODO - implement this method
-        return null;
+        for (HashMap<String, String> job : allJobs) {
+            for (String details: job.values()) {
+                    if (details.toLowerCase().contains(value.toLowerCase()) && !jobs.contains(job)){
+                        jobs.add(job);
+                    }
+
+                }
+            }
+        return jobs;
+
     }
 
     /**
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -77,7 +77,7 @@ public class TechJobs {
         Boolean validChoice = false;
         String[] choiceKeys = new String[choices.size()];
 
-        // Put the choices in an ordered structure so we can
+        // Put the choices in an ordered structure, so we can
         // associate an integer with each one
         int i = 0;
         for (String choiceKey : choices.keySet()) {
@@ -120,6 +120,19 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.print("No Results");
+        } else {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.print("\n");
+                System.out.println("*****");
+
+                for (Map.Entry<String, String> jobDetails : job.entrySet()) {
+                    System.out.println(jobDetails.getKey() + ": " + jobDetails.getValue());
+                }
+
+                System.out.println("*****");
+            }
+        }
     }
 }
